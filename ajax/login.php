04664c529cf20d03f663f572f560c753d88a43f0
Luc Hall <lucmhall@gmail.com>
2011-10-26 00:04:41 -0700

    Added login using ajax. It checks against my local database for now.

---
+++ ajax/login.php
@@ -0,0 +1,21 @@
+<?php
+
+include "../../../mysqli_connect.php";
+
+
+$un = $_POST['username'];
+$p =  $_POST['password'];
+$q= "select  id, username from users where username='$un' AND password=sha1('$p')";
+         
+       
+    //  $result = mysqli_query($dbc,$q);
+      $num = mysqli_num_rows(mysqli_query($dbc,$q));
+	if ($num == 1) {
+		$_SESSION['user'] = $un;
+		echo "1";
+		session_start();
+       }       
+                
+       
+
+?>
--- js/iLand.js
+++ js/iLand.js
@@ -1,13 +1,25 @@
 
-
-
-
 $(document).ready(function(){
 
+
+//When a user attempts to log in
 $("#loginForm").submit(function(){
+username = $("#username").val();
+password = $("#password").val();
 
 
-	window.alert("OHH yea man!");
-});
+$.post("ajax/login.php",{
+		password : ""+password+"",
+		username : ""+username+""
+		},function(data){
+	
+	if(data =="1"){
+		$("#login").fadeOut(300);	
+	}else{
+	window.alert("Invalid Username/password. Please try again");
+	}	
+		});
+		
+	});
 	
 });
