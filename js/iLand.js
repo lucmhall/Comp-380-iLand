69fb6f15a59b59433c0afe2ef3f7c0d4f1c90b3e
Luc Hall <lucmhall@gmail.com>
2011-10-26 23:51:40 -0700

    added chat. Its still linked with OrbiterMicro's server, need to change that

--- js/iLand.js
+++ js/iLand.js
@@ -1,30 +1,177 @@
+/***********Orbiter Micro*****************************/
 
-$(document).ready(function(){
-	sizeW = (window.innerWidth/2)-100;
+// Create Orbiter object
+var orbiter = new net.user1.orbiter.Orbiter();
+ 
+// Register for connection events
+orbiter.addEventListener(net.user1.orbiter.OrbiterEvent.READY, readyListener, this);
+orbiter.addEventListener(net.user1.orbiter.OrbiterEvent.CLOSE, closeListener, this);
+ 
+// Connect to Union
+orbiter.connect("tryunion.com", 80);
+ 
+// Connection event listeners
+function readyListener (e) {
+  alert("OrbiterMicro connected to Union!");
+}
+ 
+function closeListener (e) {
+  alert("OrbiterMicro connection closed.");
+}
+
+
+
+
+//==============================================================================
+
+// VARIABLES
+
+//==============================================================================
+var orbiter;
+var msgManager;
+var UPC = net.user1.orbiter.UPC;
+var roomID = "chatRoom";
+//==============================================================================
+// INITIALIZATION
+//==============================================================================
+function init () {
+  // Create the Orbiter instance, used to connect to and communicate with Union
+  orbiter = new net.user1.orbiter.Orbiter();
+  // If required JavaScript capabilities are missing, abort
+
+  if (!orbiter.getSystem().isJavaScriptCompatible()) {
+
+    displayChatMessage("Your browser is not supported.");
+
+    return;
+
+  }
+  // Register for Orbiter's connection events
+
+  orbiter.addEventListener(net.user1.orbiter.OrbiterEvent.READY, readyListener, this);
+  orbiter.addEventListener(net.user1.orbiter.OrbiterEvent.CLOSE, closeListener, this);
+
+  // Register for incoming messages from Union
+
+  msgManager = orbiter.getMessageManager();
+  msgManager.addMessageListener(UPC.JOINED_ROOM, joinedRoomListener, this);
+  msgManager.addMessageListener(UPC.CLIENT_ADDED_TO_ROOM, clientAddedListener, this);
+  msgManager.addMessageListener(UPC.CLIENT_REMOVED_FROM_ROOM, clientRemovedListener, this);
+  msgManager.addMessageListener("CHAT_MESSAGE", chatMessageListener, this, [roomID]);
+
+  // Connect to Union
+  orbiter.connect("tryunion.com", 80);
+  displayChatMessage("Connecting to Union...");
+
+}
+//==============================================================================
+
+// ORBITER EVENT LISTENERS
+//==============================================================================
+
+// Triggered when the connection is ready
+function readyListener (e) {
+  displayChatMessage("Connected.");
+  displayChatMessage("Joining chat room...");
+  // Create the chat room
+  msgManager.sendUPC(UPC.CREATE_ROOM, roomID);
+  // Join the chat room
+  msgManager.sendUPC(UPC.JOIN_ROOM, roomID);
+}
+
+// Triggered when the connection is closed
+
+function closeListener (e) {
+
+  displayChatMessage("Orbiter connection closed.");
+
+}
+//==============================================================================
+// CHAT ROOM EVENT LISTENER
+//==============================================================================
+// Triggered when a JOINED_ROOM message is received
+function joinedRoomListener () {
+  displayChatMessage("Chat ready!");
+}
+// Triggered when another client joins the chat room
+function clientAddedListener (roomID, clientID) {
+  displayChatMessage("User" + clientID + " joined the chat.");
+}
+// Triggered when another client leaves the chat room
+function clientRemovedListener (roomID, clientID) {
+  displayChatMessage("User" + clientID + " left the chat.");
+}
+//==============================================================================
+// CHAT SENDING AND RECEIVING
+//==============================================================================
+// Sends a chat message to everyone in the chat room
+function sendMessage () {
+  var outgoing = document.getElementById("outgoing");
+  if (outgoing.value.length > 0) {
+    msgManager.sendUPC(UPC.SEND_MESSAGE_TO_ROOMS, "CHAT_MESSAGE", roomID, "true", "", outgoing.value);
+    outgoing.value = "";
+    // Focus text field again after submission (required for IE8 only)
+    setTimeout(function () {outgoing.focus();}, 10);
+  }
+}
+// Triggered when a chat message is received
+function chatMessageListener (fromClientID, message) {
+  displayChatMessage("User" + fromClientID + ": " + message);
+}
+// Displays a single chat message
+function displayChatMessage (message) {
+  // Make the new chat message element
+  var msg = document.createElement("span");
+  msg.appendChild(document.createTextNode(message));
+  msg.appendChild(document.createElement("br"));
+  // Append the new message to the chat
+  var chatPane = document.getElementById("chatPane");
+  chatPane.appendChild(msg);
+  // Trim the chat to 500 messages
+  if (chatPane.childNodes.length > 500) {
+    chatPane.removeChild(chatPane.firstChild);
+  }
+  chatPane.scrollTop = chatPane.scrollHeight;
+}
+
+
+
+
+/***********Javascript Functions*****************************/
+
+//All javascript that will be run when the page loads goes here.
+function pageLoad(){
+sizeW = (window.innerWidth/2)-100;
 	sizeW = sizeW+"px";
 	sizeH = (window.innerHeight/2)-100;
 	sizeH = sizeH+"px";
-	
 	$("#loginForm").css({"margin-left":sizeW});
 	$("#loginForm").css({"margin-top":sizeH});
-
-
-
-
 	$("#login").css({"width":window.innerWidth});
 	$("#login").css({"height":window.innerHeight});
 	$("#username").focus(); 
+}
+
+/************Jquery**************************************/
+//All JQuery goes here.
+$(document).ready(function(){
+	pageLoad();
 //When a user attempts to log in
 $("#loginForm").submit(function(){
+
+//Get input username and password.
 username = $("#username").val();
 password = $("#password").val();
 
-
+//AJAX call for login script
 $.post("ajax/login.php",{
+//password/unsername passed to php script
 		password : ""+password+"",
 		username : ""+username+""
 		},function(data){
+	//Remove password from page
 	$("#password").val("");
+	//If login was correct 1 should be returned
 	if(data =="1"){
 		
 		$("#login").fadeOut(300);	
@@ -39,3 +186,4 @@ $.post("ajax/login.php",{
 	});
 	
 });
+/********************************************************/
